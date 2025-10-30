4f21a2747693a14d834c3b72c7a1fc35685a3c6e
ThomasAstete <thomas.astete@inacapmail.cl>
2025-10-30 00:48:47 -0300

    Switch to PostgreSQL and add dotenv support
    Updated database configuration to use PostgreSQL with environment variables loaded via python-dotenv. Added python-dotenv to requirements for managing environment variables securely.

--- Visitas_empresa/settings.py
+++ Visitas_empresa/settings.py
@@ -7,8 +7,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
+from dotenv import load_dotenv
 import os
-
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,8 +71,12 @@ WSGI_APPLICATION = 'Visitas_empresa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("dbname"),
+        'USER': os.getenv("user"),
+        'PASSWORD': os.getenv("password"),
+        'HOST': os.getenv("host"),
+        'PORT': os.getenv("port"),
     }
 }
 
