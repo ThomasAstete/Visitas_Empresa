d67e5a2fd9844fe1e0a8e062abe815bc6a429378
ThomasAstete <thomas.astete@inacapmail.cl>
2025-11-24 03:54:23 -0300

    Add CORS support with django-cors-headers
    Integrated 'corsheaders' into INSTALLED_APPS and MIDDLEWARE to enable CORS support. Configured allowed origins and credentials to permit requests from the specified Heroku domain.

--- Visitas_empresa/settings.py
+++ Visitas_empresa/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registro_visitas',
+    'corsheaders',
     'rest_framework'
 ]
 
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -63,6 +65,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Visitas_empresa.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://visitas-api-83b1ca9a4884.herokuapp.com",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
     {
