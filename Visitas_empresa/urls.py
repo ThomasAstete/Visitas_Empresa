019ee379770df7036eb3fce53d2389279a8f5252
ThomasAstete <thomas.astete@inacapmail.cl>
2025-11-24 19:46:49 -0300

    Update urls.py

--- Visitas_empresa/urls.py
+++ Visitas_empresa/urls.py
@@ -16,7 +16,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('registro_visitas.urls')),
     path('', include('registro_visitas.urls')),
 
 ]
