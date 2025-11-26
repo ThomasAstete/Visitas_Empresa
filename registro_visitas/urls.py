c2aefb8b36895a3f3b54e024c3249d7a67717438
ThomasAstete <thomas.astete@inacapmail.cl>
2025-11-26 00:29:41 -0300

    Add session and basic auth to DRF settings
    Included SessionAuthentication and BasicAuthentication in REST_FRAMEWORK settings to support additional authentication methods. Also reordered API route registration in urls.py for clarity.

--- registro_visitas/urls.py
+++ registro_visitas/urls.py
@@ -9,12 +9,13 @@ router.register(r"groups", views.GroupViewSet)
 router.register(r"visitas", views.VisitaViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('', views.registrar_visita, name='registrar_visita'),
     path('lista/', views.lista_visitas, name='lista_visitas'),
     path('salida/<int:visita_id>/', views.registrar_salida, name='registrar_salida'),
     path('editar/<int:visita_id>/', views.editar_visita, name='editar_visita'),
+    path('api/', include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    
     
         # Rutas JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
